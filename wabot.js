37b0e791129fd5271ca4e0902b914f59cd9da1de
Andre Pratama <151502882+andreprr@users.noreply.github.com>
2024-09-29 18:58:00 +0700

    Update wabot.js

--- wabot.js
+++ wabot.js
@@ -32,12 +32,260 @@ client.on('message', message => {
     3. !bhnmq
     4. !lvlprof
     5. !lvlalche
+    6. !upcrysta
     6. kirim masukan bisa tag @andre`;
         message.reply(response);
     }
 });
 
 // Mendengarkan pesan untuk level
+client.on('message', message => {
+    if (message.body === '!upcrysta') {
+        const response = `
+        🔮🔮DAFTAR UPGRADE CRYSTA🔮🔮
+        ✉Sumber : Player Toram
+        
+        🔵 LIST UP XTALL NORMAL 🔵
+        ===========================
+        VERSI INDONESIA
+        1. Ksatriaja -> Volotur -> Brassozard
+        2. Lobalawar -> Melancia
+        3. Nurethoth -> Guignol -> Golem Satpam
+        4. Aranea -> Blazingur
+        5. Shawle -> Dutannen
+        6. Gespenst -> Salamander -> Bullamius
+        7. Minotaur -> Rhinosaurus
+        8. Naga Beringas Decel -> York -> Tuscog -> Bayangan Hitam -> Torexesa
+        9. Metal Stinger -> Kapten Lyark Spesialis -> Ageladanios
+        10. Odelon Machina -> Pret -> Lilicarolla
+        11. Coryn Besar -> Seraph Machina -> Limacina
+        12. Crimsosh -> Amoeba Machina
+        13. Potum Raja -> Potum Platina
+        14. Flare Volg -> Charugon
+        15. Bos Petapa Kadal -> Orictoceras
+        16. Gravicep -> Naga Abu Merah Rudis
+        17. Lynx Mithurna -> Kuffania
+        
+        ===========================
+        ENGLISH VERSION
+        1. Gigan Knight -> Volotur -> Brassozard
+        2. Radibat -> Melancia
+        3. Nurethoth -> Guignol -> Guard Golem
+        4. Aranea -> Blazingur
+        5. Shawle -> Dutannen
+        6. Gespenst -> Salamander -> Bullamius
+        7. Minotour -> Rhinosaur
+        8. Brutal Dragon Decel -> York -> Tuscog -> Black Shadow -> Torexesa
+        9. Metal Stinger -> Lyark Master Specialist -> Ageladanios
+        10. Odelon Machina -> Tappler -> Lilicarolla
+        11. Big Coryn -> Seraph Machina -> Limacina
+        12. Crimsosch -> Amoeba Machina
+        13. King Potum -> Platinum Potum
+        14. Flare Volg -> Charugon
+        15. Fanadon -> Orictoceras
+        16. Gravicep -> Red Ash Dragon Rudis
+        17. Mithurna Lynx -> Kuffania
+        
+        ===========================
+        
+        🔴 LIST UP XTALL WEAPON 🔴
+        ===========================
+        VERSI INDONESIA
+        1. Metasrigala -> Cakar Kucing Kissatpam
+        2. Pomie Chan -> Pomie Chan II
+        3. Potum Pahlawan -> Potum Pahlawan II -> Potum Pahlawan III -> Potum Pahlawan IV
+        4. Ganglef -> Machina Tiran -> Vulture - Naga Penyamar Mimyugon
+        5. Armasit -> Kadal Bunga
+        6. Imitacia -> Finstern si Naga Kegelapan -> Oculasignio
+        7. Imitator -> Mardula -> Velum
+        8. Golem Pilar -> Machina Ultima -> Vlam si Naga Api
+        9. Zahhak Machina -> Penyihir Besi Bercakar -> Naga Meraung Bovinari
+        10. Iblis Kristal Jahanam -> Shampy -> Irestida -> Vatudo
+        11. Ksatria Buruk Dusta -> Gwaimol -> Hexter -> Don Profundo
+        12. Gerbang Iblis -> Mozto Machina -> Pisteus -> Naga Jahat Fazzino
+        13. Zolban -> Repthon -> Naga Trompet Reguita
+        14. Pedang Sihir Iblis -> Kristal Berlumur Darah
+        15. Mbah Dukun Usasama -> Mbah Dukun Usasama II
+        16. Ganglef -> Kepiting Bulan Besar
+        17. Dukun Lapin -> Momok Gelembung
+        18. Golem Galian -> Builder Golem
+        19. Pedang Neo Maton -> Pedrio
+        20. Zega III -> Zega IV -> Zega V -> Zega VI -> Zega VII
+        21. Sang Juara Megiston -> Sang Juara Megiston II -> Sang Juara Megiston III -> Sang Juara Megiston IV -> Sang Juara Megiston V
+        22. Arbogazella -> Doridodi
+        23. Armasit -> Diark
+        
+        ===========================
+        ENGLISH VERSION
+        1. Blancanine the White Fang -> Cat Claw Guardkissa
+        2. Pomie Chan -> Pomie Chan II
+        3. Hero Potum -> Hero Potum II -> Hero Potum III -> Hero Potum IV
+        4. Ganglef -> Tyrant Machina -> Vulture - Trickster Dragon Mimyugon
+        5. Armasite -> Florizard
+        6. Imitacia -> Finstern the Dark Dragon -> Oculasignio
+        7. Imitator -> Mardula -> Velum
+        8. Pillar Golem -> Ultimate Machina -> Vlam the Flame Dragon
+        9. Zahhak Machina -> Clawed Iron Witch -> Raging Dragon Bovinari
+        10. Evil Crystal Beast -> Shampy -> Irestida -> Vatudo
+        11. Black Knight of Delusion -> Gwaimol -> Hexter -> Don Profundo
+        12. Demon's Gate -> Mozto Machina -> Pisteus -> Wicked Dragon Fazzino
+        13. Zolban -> Repthon -> Brass Dragon Reguita
+        14. Evil Magic Sword -> Blood Smeared Crystal
+        15. Usasama the Necromancer -> Usasama the Necromancer II
+        16. Ganglef -> Giant Moon Crab
+        17. Lapin The Necromancer -> Bubble Bogey
+        18. Excavated Golem -> Builder Golem
+        19. Neo Maton Sword -> Pedrio
+        20. Zega III -> Zega IV -> Zega V -> Zega VI -> Zega VII
+        21. Megiston The Champion -> Megiston The Champion II -> Megiston The Champion III -> Megiston The Champion IV -> Megiston The Champion V
+        22. Arbogazella -> Doridodi
+        23 Armasite -> Diark
+        
+        ===========================
+        
+        🟢 LIST UP XTALL ARMOR 🟢
+        ===========================
+        VERSI INDONESIA
+        1. Cerabes -> Mimesia -> Sakura Merah Jelita -> Baavgai
+        2. Kruztor -> Kruztor II
+        3. Sosok Jahat -> Velly Hitam
+        4. Pilz Erosi -> Filrocas
+        5. Golem Preman -> Naga Langkisau
+        6. Ifrid -> Mama Fluck -> Gordel
+        7. Forestia -> Glaucius
+        8. Usakichi -> Usami -> Usamochi
+        9. Bos Roga -> Iconos -> Ornlaf -> Roga Safir -> Ferzen si Naga Batu -> Walican
+        10. Gopherga -> Yuveria
+        11. Cerberus -> Pyxtica -> Gemma
+        12. Noeliel -> Noeliel si Patung Es Suci -> Kucing Yule -> Gegner
+        13. Quasar Jahanam -> Mata Jahanam
+        14. Colon Komandan -> Goleps
+        15. Elang Zamrud -> Jamur Super Mampus
+        16. Tortuga -> Daddy Finpen -> Capo Profundo
+        17. Dr. Leonardo -> Dr. Leonardo II
+        18. DX Fighter -> DX Fighter II
+        19. Arachnidemon -> Zapo
+        
+        ===========================
+        ENGLISH VERSION
+        1. Cerabes -> Mimesia -> Dreamy Scarlet Sakura -> Baavgai
+        2. Kruztor -> Kruztor II
+        3. Evil Shadow -> Black Velly
+        4. Eroded Pilz -> Filrocas
+        5. Thug Golem -> Galegon
+        6. Ifrid -> Mom Fluck -> Gordel
+        7. Forestia -> Glaucius
+        8. Usakichi -> Usami -> Usamochi
+        9. Boss Roga -> Iconos -> Ornlaf -> Sapphire Roga -> Ferzen the Rock Dragon -> Walican
+        10. Gopherga -> Yuveria
+        11. Cerberus -> Pyxtica -> Gemma
+        12. Noeliel -> Noeliel the Ice Statue -> Yule Cat -> Gegner
+        13. Demonic Quasar -> Demonic Eye
+        14. Colon Commander -> Goleps
+        15. Jade Raptor -> Super Death Mushroom
+        16. Tortuga -> Daddy Finpen -> Capo Profundo
+        17. Dr. Leonardo -> Dr. Leonardo II
+        18. DX Fighter -> DX Fighter II
+        19. Arachnidemon -> Zapo
+        
+        ===========================
+        
+        🟡 LIST UP XTALL ADDITIONAL 🟡
+        ==============================
+        VERSI INDONESIA
+        1. Kapten Karatan -> Exdocus
+        2. Warmonger -> Proto Leon -> Raja Piton -> Naga Membara Igneus
+        3. Naga Senja -> Baphomela -> Bayangan Biru Tafakur
+        4. Naga Sabana Yelb -> Roda Kelana -> Roda Kelana Neo
+        5. Naga Sabana Yelb -> Roda Kelana -> Gordo
+        6. Pumpking -> Jeila -> Zoe -> Zarth -> Neewollah -> Eripmav
+        7. Dusk Machina -> Trokostida
+        8. Ayah Yashiro Azuki -> Raja Kegelapan
+        9. Raja Kerbau -> Paduka Raja Kerbau
+        10. Raja Kerbau -> Pillow Kitagawa
+        11. Biskuit Buatan Tangan -> Alfenix
+        12. Eidenliebe -> Garnache
+        13. Gespenst II -> Stellar Ooze
+        14. Adaro -> Monster Dasar Laut
+        15. Seltirio -> Tardigrademon
+        16. Iconos Emas -> Kodok Riang -> Gem Mas
+        17. Celeng Raksasa -> Mega Alpoca
+        18. Tengkorak Emas -> Solopy
+        19. Candela -> Candela II -> Amargon
+        20. Chocolate Ooze -> Chocolate Ooze II -> Soldner
+        21. Ratu Kuno -> Ratu Kuno II
+        22. Altoblepas -> Jiva
+        23. Grylee -> Fantica
+        
+        ==============================
+        ENGLISH VERSION
+        1. Corroded Knight Captain -> Exdocus
+        2. Warmonger -> Proto Leon -> King Piton -> Burning Dragon Igneus
+        3. Twilight Dragon -> Baphomela -> Prudent Blue Shadow
+        4. Grass Dragon Yelb -> Wandering Wheel -> Neo Wandering Wheel
+        5. Grass Dragon Yelb -> Wandering Wheel -> Gordo
+        6. Pumpking -> Jeila -> Zoe -> Zarth -> Neewollah -> Eripmav
+        7. Dusk Machina -> Trocostida
+        8. Yashiro Azuki's Dad -> Dark Lord
+        9. Ox King -> Royal Ox King
+        10. Ox King -> Pillow Kitagawa
+        11. Handmade Cookie -> Alfenix
+        12. Eidenliebe -> Garnache
+        13. Gespenst II -> Stellar Ooze
+        14. Adaro -> Underwater Ruins Monster
+        15. Seltirios -> Tardigrandemon
+        16. G. Iconos -> Felicitoad -> Goldigem
+        17. Giant Boar -> Mega Alpoca
+        18. Golden Skeleton -> Solopy
+        19. Candela -> Candela II -> Amargon
+        20. Chocolate Ooze -> Chocolate Ooze II -> Soldner
+        21. Ancient Empress -> Ancient Empress II
+        22. Altoblepas -> Jiva
+        23. Grylee -> Fantica
+        
+        ==============================
+        
+        🟣 LIST UP XTALL RING 🟣
+        =============================
+        VERSI INDONESIA
+        1. Kristal Jahat -> Tapir -> Patissia -> Sicanokami
+        2. Kristal Jahat -> Kristal Misterius
+        3. Viscum -> Deniala -> Amalgam -> Crysmort -> Breeta
+        4. Bexiz -> Zelbuse -> Naga Milisi Turba
+        5. Dark Mushroom -> Teertocrit
+        6. Baron Bling-bling -> Roh Orang Mati
+        7. Venena -> Venena II -> Humida
+        8. Violaccoon -> Ketua Bandit Gurun
+        9. Ooze -> Lalvada -> Dominaredor
+        10. Tentara Batu -> Memecoleous
+        11. Volgagon -> Etoise
+        12. Seele Zauga -> Seele Zauga II
+        13. Potumotter -> Potumotter II
+        
+        =============================
+        ENGLISH VERSION
+        1. Eerie Crystal -> Tapir -> Patissia -> Sicanokami
+        2. Eerie Crystal -> Mysterious Crystal
+        3. Viscum -> Deniala -> Amalgam -> Crysmort -> Breeta
+        4. Bexiz -> Zelbuse -> War Dragon Turba
+        5. Dark Mushroom -> Teertocrit
+        6. Shining Gentleman -> Espectro
+        7. Venena -> Venena II -> Humida
+        8. Violaccoon -> Sand Bandits Leader
+        9. Ooze -> Lalvada -> Dominaredor
+        10. Stone Mercenary -> Memecoleous
+        11. Volgagon -> Etoise
+        12. Seele Zauga -> Seele Zauga II
+        13. Potumotter -> Potumotter II
+        
+        =============================
+        `;
+        message.reply(response);
+    }
+});
+
+
+
 client.on('message', message => {
     if (message.body === '!lvlalche') {
         const response = `
